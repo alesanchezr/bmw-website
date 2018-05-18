a4ad1643269518fface37bce715a501dc4d243af
Alejandro Sanchez <aalejo@gmail.com>
2018-05-18 18:04:24 +0000

    gravity forms

--- wp-content/themes/rigo/partials/modal-request.php
+++ wp-content/themes/rigo/partials/modal-request.php
@@ -39,7 +39,7 @@
                     $display_description = false,
                     $display_inactive = false, 
                     $field_values = null, 
-                    $ajax = true, 
+                    $ajax = false, 
                     $tabindex = 1, 
                     $echo = true 
                   ); ?>
