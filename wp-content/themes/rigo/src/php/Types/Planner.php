1f2d80cbfc8b566169b3cf807f8fe9c689134bb8
Alejandro Sanchez <aalejo@gmail.com>
2018-10-17 20:43:03 +0000

    planer category

--- wp-content/themes/rigo/src/php/Types/Planner.php
+++ wp-content/themes/rigo/src/php/Types/Planner.php
@@ -4,6 +4,15 @@ namespace Rigo\Types;
 use WPAS\Types\BasePostType;
 
 class Planner extends BasePostType{
+    public static function serialize($planner, $size='small'){
+        $response = [
+            'id' => $planner -> ID,
+            'post_title' => $planner -> post_title,
+            'thumbnail' =>  wp_get_attachment_image_src( get_field('planner-img-banner', $planner -> ID),'medium_large')[0], 
+            'address' =>  get_field('planner-description', $planner -> ID),
+        ];
+        return $response;
+    }
 }
 
 ?>
