652eaa13d770d4436b2fc88819676d761e18c851
Alejandro Sanchez <aalejo@gmail.com>
2018-10-18 22:07:13 +0000

    herflang

--- wp-content/themes/rigo/partials/meta-tags.php
+++ wp-content/themes/rigo/partials/meta-tags.php
@@ -1,6 +1,4 @@
 <?php $args = wpas_get_view_data(); ?>
-<meta http-equiv="content-language" content="es">
-<link rel="alternate" href="https://bestmiamiweddings.com/" hreflang="en"/>
 <?php if(is_page('gallery')){ ?>
 <meta name="description" content="<?php echo get_the_title($args['venue']['ID']) ?> Gallery Pictures and Photos: <?php echo $args['venue']['venue-description'] ?>">
 <?php } ?>
